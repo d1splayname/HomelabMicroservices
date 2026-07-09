5a83cb42b9c7d94bc610ed81025e941006ff50ef
Joshua Chen <jzc0289@auburn.edu>
2026-07-09 11:31:58 -0500

    refactor: move cpp bindings to dedicated file

---
+++ cpp/bindings.cpp
@@ -0,0 +1,9 @@
+#include <pybind11/pybind11.h>
+
+int add(int a, int b);
+std::string sha256sum(std::string input);
+
+PYBIND11_MODULE(cppmethods, m) {
+    m.def("add", &add);
+    m.def("sha256sum", &sha256sum);
+}
