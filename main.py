b3cd701d8b5aa08e9ecd8acdd27c525503fc6cda
Joshua Chen <jzc0289@auburn.edu>
2026-07-09 00:35:35 -0500

    feat: update endpoint for sha256sum

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def GetUuid():
     id = uuid.uuid4()
     return {"uuid": id}
 
-@app.get("/openssl/SHA256sum")
-def SHA256Sum(input="123"):
+@app.get("/openssl/sha256sum")
+def SHA256Sum(input: str = "123"):
     result = input
 
-    return {"SHA256sum": result}
+    return {"sha256sum": cppmethods.sha256sum(input)}
 
 @app.get("/openssl/keypairgen")
 def GenerateKeyPair(input="123"):
