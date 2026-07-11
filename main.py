611551ac7fe5e4e6ca17187343f7ee0cb286554e
Joshua Chen <jzc0289@auburn.edu>
2026-07-11 00:15:14 -0500

    fix: remove keypairgen

--- main.py
+++ main.py
@@ -21,15 +21,12 @@ def GetUuid():
     id = uuid.uuid4()
     return {"uuid": id}
 
-@app.get("/openssl/sha256sum")
+@app.get("/hash/sha256sum")
 def SHA256Sum(input: str = ""):
     output = cppmethods.sha256sum(input)
 
     return {"input": input, "output": output}
 
-@app.get("/openssl/keypairgen")
-def GenerateKeyPair(usefulText: str = "USEFUL_NOTE"):
-    return {"pub.key": "-1", "key.pem": "-1"}
 
 @app.get("/add")
 def Add(a: int = 1, b: int = 1):
