f7c3a615df4b0de09145daa816e1da80981ea88d
Joshua Chen <jzc0289@auburn.edu>
2026-08-22 16:05:31 -0500

    fix: fix scripts to run in scripts folder

--- scripts/configurecmake.sh
+++ scripts/configurecmake.sh
@@ -1,3 +1,6 @@
-cmake -S . -B build \
-    -Dpybind11_DIR=$(python3 -m pybind11 --cmakedir) \
-    -DCMAKE_INSTALL_PREFIX=$(python3 -c "import site; print(site.getsitepackages()[0])")
+BASE_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
+PYTHON_BIN="$BASE_DIR/.venv/bin/python"
+PYBIND11_DIR="$("$PYTHON_BIN" -m pybind11 --cmakedir)"
+INSTALL_PREFIX="$("$PYTHON_BIN" -c "import site; print(site.getsitepackages()[0])")"
+
+cmake -S "$BASE_DIR" -B "$BASE_DIR/build" -Dpybind11_DIR="$PYBIND11_DIR" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX"
