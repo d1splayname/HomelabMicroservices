0730718b03d1fa39917cf1b880d85e65b1d76fa3
Joshua Chen <jzc0289@auburn.edu>
2026-08-22 16:24:02 -0500

    fix: flagtypo

--- scripts/configurecmake.sh
+++ scripts/configurecmake.sh
@@ -3,4 +3,4 @@ PYTHON_BIN="$BASE_DIR/.venv/bin/python"
 PYBIND11_DIR="$("$PYTHON_BIN" -m pybind11 --cmakedir)"
 INSTALL_PREFIX="$("$PYTHON_BIN" -c "import site; print(site.getsitepackages()[0])")"
 
-cmake -S "$BASE_DIR" -B "$BASE_DIR/build" -Dpybind11_DIR="$PYBIND11_DIR" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -Who-dev
+cmake -S "$BASE_DIR" -B "$BASE_DIR/build" -Dpybind11_DIR="$PYBIND11_DIR" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -Wno-dev
