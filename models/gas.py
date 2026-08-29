59f3c6cf1c8d76caf05f171a96e48b4230183cb0
Joshua Chen <jzc0289@auburn.edu>
2026-08-29 12:25:30 -0500

    feat: add model and router for gas logging

---
+++ models/gas.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, text
+
+from routers.gas import GasBase
+
+class Gas(GasBase):
+    __tablename__ = "gas"
+
+    id: Mapped[int] = mapped_column(
+        primay_key=True,
+        nullable=False
+    )
+
+    gas: Mapped[float] = mapped_column(
+        nullable=False
+    )
+
+    timestamp: Mapped[str] = mapped_column(
+        String(32),
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP()")
+    )
---
+++ routers/gas.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+import os
+
+load_dotenv()
+
+_DB_HOST = os.getenv("DB_HOST")
+_DB_PORT = os.getenv("DB_PORT")
+_DB_USERNAME = os.getenv("DB_USER")
+_DB_PASSWORD = os.getenv("DB_PASS")
+_GAS_DB_DATABASE = os.getenv("LOG_DB_DATABASE")
+
+_DATABASE_URL = (
+    "mariadb+mariadbconnector://"
+    f"{quote_plus(_DB_USERNAME)}:{quote_plus(_DB_PASSWORD)}"
+    f"{_DB_HOST}:{_DB_PORT}/{_GAS_DB_DATABASE}"
+)
+
+gasEngine = create_engine(
+    _DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+)
+
+SessionLocal = sessionmaker (
+    bind=gasEngine,
+    autoFlush=False,
+    expire_on_commit=False,
+)
+
+class GasBase(DeclarativeBase):
+    pass
+
+def GasGetDB():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
